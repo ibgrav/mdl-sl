6461630c40cc596fbb76eb8cadda41099ab9ada3
ibgrav <45953867+ibgrav@users.noreply.github.com>
2019-05-27 18:54:29 -0400

    sls

--- handler.js
+++ handler.js
@@ -1,14 +1,41 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+const request = require('request');
+const token = "Crusty5oil!";
+const url = "http://54.197.196.151/v1/graphql";
+
+module.exports.query = async (event) => {
+  let q = decodeURI(event.pathParameters.q);
+  //let q = "{ing{name type}}";
+  let data = {
+    "query": q
+  }
+
+  let getData = await hasuraPost(data);
+  return getData;
+};
+
+function hasuraPost(data) {
+  return new Promise((resolve, reject) => {
+    request({
+      url: url,
+      method: "POST",
+      headers: { 'x-hasura-admin-secret': token },
+      json: true,
+      body: data
+    }, function (error, response, body) {
+      if (error) reject(rtn(400, error));
+      else if(body.errors) reject(rtn(400, body.errors));
+      else resolve(rtn(200, body));
+    });
+  });
+}
+
+function rtn(status, returnMsg){
   return {
-    statusCode: 200,
+    statusCode: status,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message:  returnMsg
     }, null, 2),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+}
